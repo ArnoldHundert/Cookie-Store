0bd9538f11c1dfa1c5c6b132e650cbe9d2f8f2ef
Arnold Hundert <hunderta@yahoo.com>
2017-02-06 17:50:32 -0500

    Finished.

--- js/main.js
+++ js/main.js
@@ -34,25 +34,50 @@ window.addEventListener("load",function(){
 		Cookies.set("Chocolate", "0");
 		Cookies.set("Lemon", "0");
 
+		showOrderFunction();
+	}
+	function showOrderFunction() {
 		document.getElementById("sugarOrder").innerHTML = Cookies.get("Sugar");
 		document.getElementById("chocolateOrder").innerHTML = Cookies.get("Chocolate");
 		document.getElementById("lemonOrder").innerHTML = Cookies.get("Lemon");
 	}
-	document.getElementById("usrInput").addEventListener("click", function(){
-		// var sugarCookie = parseInt(document.getElementById("sugarAmt").innerHTML);
-		// var chocolateCookie = parseInt(document.getElementById("chocAmt").innerHTML);
-		// var lemonCookie = parseInt(document.getElementById("lemAmt").innerHTML);
+	document.getElementById("orderInput").addEventListener("click", function(){
+		var sugarCookieOrder = parseInt(document.getElementById("sugarAmt").value);
+		var chocolateCookieOrder = parseInt(document.getElementById("chocolateAmt").value);
+		var lemonCookieOrder = parseInt(document.getElementById("lemonAmt").value);
+		if (isNaN(sugarCookieOrder) == false) {
+			var oldSugarNum = parseInt(Cookies.get("Sugar"));	// Get old order amount
+			var newSugarOrderAmt = sugarCookieOrder + oldSugarNum;	// Add it to new order amount
+			Cookies.set("Sugar", newSugarOrderAmt);	// Set cookie to new order value
+			showOrderFunction();	// Run function to update order amount in display
+		}
+		if (isNaN(chocolateCookieOrder) == false) {
+			var oldchocolateNum = parseInt(Cookies.get("Chocolate"));	// Get old order amount
+			var newChocolateOrderAmt = chocolateCookieOrder + oldchocolateNum;	// Add it to new order amount
+			Cookies.set("Chocolate", newChocolateOrderAmt);	// Set cookie to new order value
+			showOrderFunction();	// Run function to update order amount in display
+		}
+		if (isNaN(lemonCookieOrder) == false) {
+			var oldlemonNum = parseInt(Cookies.get("Lemon"));	// Get old order amount
+			var newLemonOrderAmt = lemonCookieOrder + oldlemonNum;	// Add it to new order amount
+			Cookies.set("Lemon", newLemonOrderAmt);	// Set cookie to new order value
+			showOrderFunction();	// Run function to update order amount in display
+		}
 
-	// 	if(!Cookies.get("Sugar")) {
-	// 		Cookies.set("Sugar", sugarCookie);
-	// 	}else{
-	// 		Cookies.set("Sugar", parseInt(Cookies.get("Sugar")) + sugarCookie);
-	// 	}
-	// })
-	// document.getElementById("usrInput").addEventListener("click", function(){
-
+		document.getElementById("sugarAmt").value = "";
+		document.getElementById("chocolateAmt").value = "";
+		document.getElementById("lemonAmt").value = "";
+		document.getElementById("sugarAmt").focus();
+	})
+	document.getElementById("clrOrderInput").addEventListener("click", function(){
+		myFunction();
 	})
 })  
+
+
+
+
+
 
 
 

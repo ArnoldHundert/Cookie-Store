c6b14b73abc7a23b305a6a916d0067b615af0e2b
Arnold Hundert <hunderta@yahoo.com>
2017-02-07 17:45:12 -0500

    Added 2 required features that I previously missed.

--- js/main.js
+++ js/main.js
@@ -20,26 +20,30 @@
 // * Thoughtful styling added
 // * Interactive and fun for the user to use
 // * A user should be able to:
-// 	* Grab a cookie flavor, which increments their cookie count by one
-// 	* Come back to the page and see how many cookies they've grabbed so far
-// 	* Clear their cookie count completely in case they're feeling guilty
+// 	1. Grab a cookie flavor, which increments their cookie count by one.
+// 	2. Come back to the page and see how many cookies they've grabbed so far.
+// 	3. Clear their cookie count completely in case they're feeling guilty.
 
 
 window.addEventListener("load",function(){
 	myFunction();
 	function myFunction() {
-		document.getElementById("sugarAmt").focus();
-
-		Cookies.set("Sugar", "0");
-		Cookies.set("Chocolate", "0");
-		Cookies.set("Lemon", "0");
-
+		if (isNaN(Cookies.get("Sugar")) == true){
+			Cookies.set("Sugar", "0", "expires=Wed, 15 Mar 2022 11:23:41 UTC");
+		}
+		if (isNaN(Cookies.get("Chocolate")) == true){
+			Cookies.set("Chocolate", "0", "expires=Wed, 15 Mar 2022 11:23:41 UTC");
+		}
+		if (isNaN(Cookies.get("Lemon")) == true){
+			Cookies.set("Lemon", "0", "expires=Wed, 15 Mar 2022 11:23:41 UTC");
+		}
 		showOrderFunction();
 	}
 	function showOrderFunction() {
 		document.getElementById("sugarOrder").innerHTML = Cookies.get("Sugar");
 		document.getElementById("chocolateOrder").innerHTML = Cookies.get("Chocolate");
 		document.getElementById("lemonOrder").innerHTML = Cookies.get("Lemon");
+		document.getElementById("sugarAmt").focus();
 	}
 	document.getElementById("orderInput").addEventListener("click", function(){
 		var sugarCookieOrder = parseInt(document.getElementById("sugarAmt").value);
@@ -70,9 +74,33 @@ window.addEventListener("load",function(){
 		document.getElementById("sugarAmt").focus();
 	})
 	document.getElementById("clrOrderInput").addEventListener("click", function(){
+		Cookies.set("Sugar", "0", "expires=Wed, 15 Mar 2022 11:23:41 UTC");
+		Cookies.set("Chocolate", "0", "expires=Wed, 15 Mar 2022 11:23:41 UTC");
+		Cookies.set("Lemon", "0", "expires=Wed, 15 Mar 2022 11:23:41 UTC");
 		myFunction();
 	})
+	document.getElementById("sugarPlusOne").addEventListener("click", function(){
+		var oldSugarNum = parseInt(Cookies.get("Sugar"));	// Get old order amount
+		var newSugarOrderAmt = oldSugarNum + 1;	// Add 1 to it
+		Cookies.set("Sugar", newSugarOrderAmt);	// Set cookie to new order value
+		showOrderFunction();	// Run function to update order amount in display
+	})
+	document.getElementById("chocolatePlusOne").addEventListener("click", function(){
+		var oldchocolateNum = parseInt(Cookies.get("Chocolate"));	// Get old order amount
+		var newChocolateOrderAmt = oldchocolateNum + 1;	// Add 1 to it
+		Cookies.set("Chocolate", newChocolateOrderAmt);	// Set cookie to new order value
+		showOrderFunction();	// Run function to update order amount in display
+	})
+	document.getElementById("lemonPlusOne").addEventListener("click", function(){
+		var oldLemonNum = parseInt(Cookies.get("Lemon"));	// Get old order amount
+		var newLemonOrderAmt = oldLemonNum + 1;	// Add 1 to it
+		Cookies.set("Lemon", newLemonOrderAmt);	// Set cookie to new order value
+		showOrderFunction();	// Run function to update order amount in display
+	})
 })  
+
+
+
 
 
 
